3a102fdaaa52b6876296194e7c98195c712636a0
Anu790j <91656728+Anu790j@users.noreply.github.com>
2021-10-07 03:35:45 -0700

    Add files via upload

---
+++ surya.c
@@ -0,0 +1,19 @@
+c
+#include<iostream.h>
+#include<stdio.h>
+
+int main()
+{
+    int i,int array[50], int find;
+    printf("Enter the item to search" );
+    scanf("%d",find);
+    for(i=0;i<50;i++)
+    {
+        find==array[i];
+        printf("%d Found Item is:, find);
+    }
+    else
+    {
+        printf("The item does not exist in an array");
+    }
+}
